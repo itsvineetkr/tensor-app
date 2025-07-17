a526ae327d0bf22bf305f32757ffd96f2c2f1d54
Vineet Kumar <itsvineetkr@gmail.com>
2025-07-17 13:17:17 +0530

    base url

--- app/root.jsx
+++ app/root.jsx
@@ -19,6 +19,7 @@ export default function App() {
   return (
     <html>
       <head>
+        <base href="/parked/" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
--- vite.config.js
+++ vite.config.js
@@ -38,6 +38,7 @@ if (host === "localhost") {
 }
 
 export default defineConfig({
+  base: "/parked/",
   server: {
     allowedHosts: [host],
     cors: {
